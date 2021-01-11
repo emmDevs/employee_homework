c8e720b7b18bc2d14a62a7d5787f174d02430d6c
Emma Hack <ehack78@gmail.com>
2021-01-11 19:34:24 +0000

    Database admin all tests passed

--- src/main/java/staff/techStaff/DatabaseAdmin.java
+++ src/main/java/staff/techStaff/DatabaseAdmin.java
@@ -1,4 +1,10 @@
 package staff.techStaff;
 
-public class DatabaseAdmin {
+import staff.Employee;
+
+public class DatabaseAdmin extends Employee {
+
+    public DatabaseAdmin(String name, String nINumber, Double salary){
+        super(name, nINumber, salary);
+    }
 }
--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.DatabaseAdmin;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseAdminTest {
+
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void setUp(){
+        databaseAdmin = new DatabaseAdmin("Courtney Cox", "FG6638923Z", 32768.83);
+
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Courtney Cox", databaseAdmin.getName());
+    }
+
+    @Test
+    public void getNINumber(){
+        assertEquals("FG6638923Z", databaseAdmin.getnINumber());
+    }
+
+    @Test
+    public void getSalary(){
+        assertEquals(32768.83, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        databaseAdmin.raisesalary(5);
+        assertEquals(34407.27, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetBonus(){
+        assertEquals(327.69, databaseAdmin.payBonus(), 0.01);
+    }
 }
