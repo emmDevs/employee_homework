fb931343b41e8638e7bc3fafcac88018d5812163
Emma Hack <ehack78@gmail.com>
2021-01-11 19:29:09 +0000

    all developer tests passed

--- src/main/java/staff/techStaff/Developer.java
+++ src/main/java/staff/techStaff/Developer.java
@@ -1,4 +1,10 @@
 package staff.techStaff;
 
-public class Developer {
+import staff.Employee;
+
+public class Developer extends Employee {
+
+    public Developer(String name, String nINumber, Double salary){
+        super(name, nINumber, salary);
+    }
 }
--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.Employee;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+
+     Developer developer;
+
+    @Before
+    public void setUp(){
+        developer = new Developer("Joey Tribiani", "PD654321T", 25341.50);
+
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Joey Tribiani", developer.getName());
+    }
+
+    @Test
+    public void getNINumber(){
+        assertEquals("PD654321T", developer.getnINumber());
+    }
+
+    @Test
+    public void getSalary(){
+        assertEquals(25341.50, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        developer.raisesalary(5);
+        assertEquals(26608.58, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetBonus(){
+        assertEquals(253.42, developer.payBonus(), 0.01);
+    }
 }
